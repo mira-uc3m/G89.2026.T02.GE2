a67de141694c7e7f1cb559db6dd61e86d9e03020
mira-uc3m <100589386@alumnos.uc3m.es>
2026-02-26 16:20:37 +0100

    First 2 Invalid test cases and method defintion

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -5,6 +5,10 @@ class EnterpriseManager:
     def __init__(self):
         pass
 
+    def register_project(self, company_cif: str, project_achronym: str, project_description: str, department: str, date: str, budget: float):
+        """Method for registering the project"""
+        pass
+
     @staticmethod
     def validate_cif(cif: str):
         """RETURNs TRUE IF THE IBAN RECEIVED IS VALID SPANISH IBAN,
--- src/unittest/python/test_register_project_tests.py
+++ src/unittest/python/test_register_project_tests.py
@@ -1,13 +1,29 @@
-"""class for testing the regsiter_order method"""
+"""class for testing the register_project method"""
 import unittest
-from uc3m_consulting import EnterpriseManager
+import json
+import os
+from src.main.python.uc3m_consulting import EnterpriseManager, EnterpriseManagementException
 
-class MyTestCase(unittest.TestCase):
-    """class for testing the register_order method"""
-    def test_something( self ):
-        """dummy test"""
-        self.assertEqual(True, False)
+class TestRegisterProject(unittest.TestCase):
+    def setUp(self):
+        """Clean up the JSON file before each test."""
+        self.filename = "corporate_operations.json"
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
 
+    def test_TC5_invalid_cif_not_string(self):
+        """TC5: CIF must be a string."""
+        enterprise_manager = EnterpriseManager()
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(123456789, "PRO02", "first test", "LOGISTICS", "02/01/2026", 60000.01)
+        self.assertEqual(str(cm.exception), "CIF must be a string")
+
+    def test_TC6_invalid_cif_algorithm(self):
+        """TC6: CIF fails validation algorithm pattern."""
+        enterprise_manager = EnterpriseManager()
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project("AA123456B", "PRO03", "first test", "LOGISTICS", "03/01/2026", 60000.02)
+        self.assertEqual(str(cm.exception), "CIF does not pass validation algorithm")
 
 if __name__ == '__main__':
     unittest.main()
